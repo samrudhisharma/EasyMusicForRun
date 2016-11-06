95cf15e89e50d352b4d6592a46b8c98cf5466ab2
samrudhisharma <ssharm17@.ncsu.edu>
2016-11-06 10:25:03 -0500

    Accelerometer added

--- app/src/main/java/easymusicforrun/easymusicforrun/MusicPlaying.java
+++ app/src/main/java/easymusicforrun/easymusicforrun/MusicPlaying.java
@@ -6,11 +6,23 @@ import android.content.Intent;
 import android.content.ActivityNotFoundException;
 import android.net.Uri;
 
+import android.app.PendingIntent;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.app.Notification;
+import android.location.Location;
+
 /**
  * Created by samrudhi on 11/6/16.
  */
 public class MusicPlaying extends AppCompatActivity {
 
+    private SensorManager sensorManager;
+    boolean speed_condition = true;
     private UserProfileObj userProfileObj = new UserProfileObj();
 
     @Override
@@ -18,7 +30,21 @@ public class MusicPlaying extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_musicplaying);
 
-        watchYoutubeVideo("AQ-P5RR7r40");
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+
+
+        Sensor AccelometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if (AccelometerSensor != null) {
+            sensorManager.registerListener(
+                    AccelometerSensorListener,
+                    AccelometerSensor,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+
+        if(speed_condition) {
+            //TODO: Add Channel Code
+            watchYoutubeVideo("AQ-P5RR7r40");
+        }
     }
 
     public void watchYoutubeVideo(String id){
@@ -31,4 +57,27 @@ public class MusicPlaying extends AppCompatActivity {
             startActivity(webIntent);
         }
     }
+
+    private final SensorEventListener AccelometerSensorListener
+            = new SensorEventListener() {
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+
+                //Use POJO
+                if (event.values[1] <= -0.01) {
+                    speed_condition = true;
+                }
+            }
+        }
+    };
+
+
 }
