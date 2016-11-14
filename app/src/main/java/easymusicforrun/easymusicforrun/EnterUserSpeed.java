372ece19e9196a25f3e34824e0555974adb17d22
samrudhisharma <ssharm17@.ncsu.edu>
2016-11-13 21:35:49 -0500

    Get Values

--- app/src/main/java/easymusicforrun/easymusicforrun/EnterUserSpeed.java
+++ app/src/main/java/easymusicforrun/easymusicforrun/EnterUserSpeed.java
@@ -12,5 +12,7 @@ public class EnterUserSpeed extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_enteruserspeed);
+
+
     }
 }
--- app/src/main/java/easymusicforrun/easymusicforrun/MusicPlaylist.java
+++ app/src/main/java/easymusicforrun/easymusicforrun/MusicPlaylist.java
@@ -2,15 +2,35 @@ package easymusicforrun.easymusicforrun;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-/**
- * Created by samrudhi on 11/6/16.
- */
 public class MusicPlaylist extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_musicplaylist);
+        
+        Button button10 = (Button) findViewById(R.id.button10);
+        button10.setOnClickListener(new OnClickListener() {
+
+            final EditText runningId   = (EditText)findViewById(R.id.editText);
+            final EditText walkingId   = (EditText)findViewById(R.id.editText2);
+
+            public void onClick(View v)
+            {
+                final String _runningId = runningId.getText().toString();
+                final String _walkingId = walkingId.getText().toString();
+
+                System.out.println("samrudhi sharma: "+_runningId);
+                System.out.println("samrudhi sharma: "+_walkingId);
+
+            }
+        });
+
+
     }
 }
